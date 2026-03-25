2a499dd953ba7e736d44a83c7e49ac4af0298a64
Rad2007 <tiry2ooo@gmail.com>
2026-03-25 11:46:16 +0200

    ex5, cargo run

---
+++ results/5/Apple&Orange.rs
@@ -0,0 +1,65 @@
+use std::io::{self, BufRead};
+
+fn countApplesAndOranges(s: i32, t: i32, a: i32, b: i32, apples: &[i32], oranges: &[i32]) {
+    let mut apples_count = 0;
+    for &d in apples {
+        let pos = a + d;
+        if pos >= s && pos <= t {
+            apples_count += 1;
+        }
+    }
+
+    let mut oranges_count = 0;
+    for &d in oranges {
+        let pos = b + d;
+        if pos >= s && pos <= t {
+            oranges_count += 1;
+        }
+    }
+
+    println!("{}", apples_count);
+    println!("{}", oranges_count);
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let first_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let s = first_multiple_input[0].trim().parse::<i32>().unwrap();
+    let t = first_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let second_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let a = second_multiple_input[0].trim().parse::<i32>().unwrap();
+    let b = second_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let third_multiple_input: Vec<String> = stdin_iterator.next().unwrap().unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let _m = third_multiple_input[0].trim().parse::<i32>().unwrap();
+    let _n = third_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let apples: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    let oranges: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+
+    countApplesAndOranges(s, t, a, b, &apples, &oranges);
+}
