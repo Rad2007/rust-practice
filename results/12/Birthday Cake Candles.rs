6a53ddbc492cf9f614ee35985bc2e45b1af5c800
Rad2007 <tiry2ooo@gmail.com>
2026-05-04 10:38:32 +0300

    rust-12

---
+++ results/12/Birthday Cake Candles.rs
@@ -0,0 +1,40 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+fn birthdayCakeCandles(candles: &[i32]) -> i32 {
+    let mut max = candles[0];
+    for &c in candles {
+        if c > max {
+            max = c;
+        }
+    }
+
+    let mut count = 0;
+    for &c in candles {
+        if c == max {
+            count += 1;
+        }
+    }
+
+    count
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let _candles_count = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let candles: Vec<i32> = stdin_iterator.next().unwrap().unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = birthdayCakeCandles(&candles);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
